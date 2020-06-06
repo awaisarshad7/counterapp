0255c1cb6d33561084469c3a6be03eecfbb7ed47
awais arshad <aawais404.4@gmail.com>
2020-06-06 18:17:53 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,10 +9,11 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <br>
-         Updated By Awais
-        </br>
+          <br/>        
+          Updated By Awais 
+          </p>
+         
+       
         <a
           className="App-link"
           href="https://reactjs.org"
